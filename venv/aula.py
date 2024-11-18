cdbae2e717db66a09ce4f31910ff29d4cc8fe57b
Camilla Rodrigues <camilla.rodrigues.de.almeida@gmail.com>
2024-11-18 20:24:10 -0300

    Aula Contadores

--- venv/aula.py
+++ venv/aula.py
@@ -1,3 +1,31 @@
-import sqlite3
+import flet as ft
 
-print('Olá, mundo!')
+def main(page: ft.Page):
+    page.title = 'Contador'
+    page.vertical_alignment = 'center'
+
+    text_number = ft.TextField(value='0', text_align=ft.TextAlign.CENTER, width=100)
+
+    def minus_click(e):
+        text_number.value = str(int(text_number.value) - 1)
+        text_number.update()
+
+    def sum_click(e):
+        text_number.value = str(int(text_number.value) + 1)
+        text_number.update()
+
+    page.add(
+        ft.Row(
+            controls=[
+                ft.IconButton(icon=ft.icons.REMOVE, on_click=minus_click),
+                text_number,
+                ft.IconButton(icon=ft.icons.ADD, on_click=sum_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+    page.update()
+
+
+ft.app(target=main)
